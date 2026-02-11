f85207c300a19aec387d4c7e4dbaf67da02a6e60
Sudhiiishana <msudhishana@gmail.com>
2026-02-11 12:35:45 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,8 @@ import Login from "./Components/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" elements={<Login />} />
-      <Route path="/dashboard" elements={<Dashboard />} />
-    </Routes>
-  </BrowserRouter>;
+  <return>
+    <h1>Welcome to my website</h1>
+  </return>;
 }
 export default App();
